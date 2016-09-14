c82647fe8bcde908184fcf0e9b6e39b6b233e20d
Mitlond <comp122012016@mail.ru>
2016-09-14 10:02:40 +0300

    Zadanie

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+console.log(
+  typeof [],             //'object'
+  typeof {},             //'object'
+  typeof new Zerrov       //'object'
+);
+
+function search() {
+ 
+var obj = search();
+ 
+if (typeof obj === "object") { 
+    obj.method();
+}
+}
+console.log(obj);
+//привел два варианта с типом объекта и typeof
+
+var objSel = "arr_"+send_obj.value;
+  if (typeof eval(objSel) != "undefined") {
+	alert("есть")
+  } else {
+	alert("нет")
+  }
+  console.log(alert);
